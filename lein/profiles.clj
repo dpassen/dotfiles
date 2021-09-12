810e7179947aa57a55750a2be5c2500256ce3e92
Derek Passen <dpassen1@gmail.com>
2021-09-12 13:33:37 -0500

    lein: Bump test-refresh

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user
  {:plugins
   [[com.github.liquidz/antq           "1.0.0"]
-   [com.jakemccrary/lein-test-refresh "0.24.1"]
+   [com.jakemccrary/lein-test-refresh "0.25.0"]
    [lein-try                          "0.4.3"]]
   :test-refresh {:quiet true}}}
