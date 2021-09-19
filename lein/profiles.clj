d89f262c816a8d162bc7b296372697229d81b0a6
Derek Passen <dpassen1@gmail.com>
2021-09-18 19:39:45 -0500

    lein: Use group for lein-try

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,5 +2,5 @@
  {:plugins
   [[com.github.liquidz/antq           "1.1.0"]
    [com.jakemccrary/lein-test-refresh "0.25.0"]
-   [lein-try                          "0.4.3"]]
+   [lein-try/lein-try                 "0.4.3"]]
   :test-refresh {:quiet true}}}
