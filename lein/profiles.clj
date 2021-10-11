8e476e808877696921311ea4fcc7b31f4558d381
Derek Passen <dpassen1@gmail.com>
2021-10-10 21:59:43 -0500

    lein: Formatting

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user
  {:plugins
-  [[com.github.liquidz/antq           "1.1.0"]
+  [[com.github.liquidz/antq "1.1.0"]
    [com.jakemccrary/lein-test-refresh "0.25.0"]
-   [lein-try/lein-try                 "0.4.3"]]
+   [lein-try/lein-try "0.4.3"]]
   :test-refresh {:quiet true}}}
