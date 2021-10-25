81568574b3892b7fd3f3b72842390c767d72c693
Derek Passen <dpassen1@gmail.com>
2021-10-25 13:26:32 -0500

    clojure: Add nvd

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,5 +2,6 @@
  {:plugins
   [[com.github.liquidz/antq "1.1.0"]
    [com.jakemccrary/lein-test-refresh "0.25.0"]
+   [lein-nvd/lein-nvd "1.7.0"]
    [lein-try/lein-try "0.4.3"]]
   :test-refresh {:quiet true}}}
