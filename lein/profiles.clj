5c00e0a8d43296479e0637e587bbf7d37773e6ba
Derek Passen <dpassen1@gmail.com>
2021-11-18 12:27:10 -0600

    clojure: Bump antq and nvd

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user
  {:plugins
-  [[com.github.liquidz/antq "1.2.0"]
+  [[com.github.liquidz/antq "1.3.0"]
    [com.jakemccrary/lein-test-refresh "0.25.0"]
-   [lein-nvd/lein-nvd "1.7.0"]
+   [lein-nvd/lein-nvd "1.8.0"]
    [lein-try/lein-try "0.4.3"]]
   :test-refresh {:quiet true}}}
