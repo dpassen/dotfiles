9c348be126d1b2e14a3ef451271144f77ffb00c1
Derek Passen <dpassen1@gmail.com>
2021-11-19 13:19:15 -0600

    clojure: Bump nvd

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,6 +2,6 @@
  {:plugins
   [[com.github.liquidz/antq "1.3.0"]
    [com.jakemccrary/lein-test-refresh "0.25.0"]
-   [lein-nvd/lein-nvd "1.8.0"]
+   [lein-nvd/lein-nvd "1.9.0"]
    [lein-try/lein-try "0.4.3"]]
   :test-refresh {:quiet true}}}
