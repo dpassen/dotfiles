56250c15dc143d2daee23e3e9f61a891af0c02cf
Derek Passen <dpassen1@gmail.com>
2021-12-23 15:37:53 -0600

    clojure: Remove nvd until I grok it again

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,6 +2,5 @@
  {:plugins
   [[com.github.liquidz/antq "1.3.1"]
    [com.jakemccrary/lein-test-refresh "0.25.0"]
-   [lein-nvd/lein-nvd "2.0.0"]
    [lein-try/lein-try "0.4.3"]]
   :test-refresh {:quiet true}}}
