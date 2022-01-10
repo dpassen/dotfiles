823312b9a11013fdb3bcfdba37112a8a05011962
Derek Passen <dpassen1@gmail.com>
2022-01-10 14:04:16 -0600

    lein: Use :changes-only for test-refresh

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,4 +3,6 @@
   [[com.github.liquidz/antq "1.3.1"]
    [com.jakemccrary/lein-test-refresh "0.25.0"]
    [lein-try/lein-try "0.4.3"]]
-  :test-refresh {:quiet true}}}
+  :test-refresh
+  {:changes-only true
+   :quiet        true}}}
