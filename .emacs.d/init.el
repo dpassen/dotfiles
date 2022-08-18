346698b18873a6573d0b47e13ac4610f93b51fa0
Derek Passen <dpassen1@gmail.com>
2022-08-18 10:54:54 -0500

    emacs: Some theme customization

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -155,7 +155,8 @@
   :global-minor-mode t)
 
 (leaf modus-themes
-  :custom (modus-themes-region . '(accented bg-only))
+  :custom ((modus-themes-mode-line . '(borderless))
+           (modus-themes-region . '(accented bg-only)))
   :init
   (pcase (plist-get (mac-application-state) :appearance)
     ("NSAppearanceNameAqua" (load-theme 'modus-operandi))
