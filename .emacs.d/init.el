a13eb0d3d3fd627484734dd4d7eeeec2faa08410
Derek Passen <dpassen1@gmail.com>
2022-12-20 09:10:40 -0600

    emacs: Add NonGNU elpa as a package source

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,8 @@
 (eval-and-compile
   (customize-set-variable
    'package-archives '(("melpa" . "https://melpa.org/packages/")
-                       ("gnu" . "https://elpa.gnu.org/packages/")))
+                       ("gnu" . "https://elpa.gnu.org/packages/")
+                       ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
   (package-initialize)
   (unless (package-installed-p 'leaf)
     (package-refresh-contents)
