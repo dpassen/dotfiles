e6166a2fbe260456977d380ea87e636a79f37837
Derek Passen <dpassen1@gmail.com>
2023-03-04 17:04:06 -0600

    clojure: Bump antq

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user
  {:plugins
-  [[com.github.liquidz/antq "2.2.999"]
+  [[com.github.liquidz/antq "2.2.1011"]
    [com.jakemccrary/lein-test-refresh "0.25.0"]
    [lein-try/lein-try "0.4.3"]]
   :test-refresh
