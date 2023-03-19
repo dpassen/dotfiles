8e36795b5c05971ad8edd79a2f2d407ec1f46957
Derek Passen <dpassen1@gmail.com>
2023-03-19 14:41:59 -0500

    wezterm: Alphabetize table

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,9 +9,10 @@ function scheme_for_appearance(appearance)
 end
 
 return {
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 	font = wezterm.font("PragmataPro"),
-	line_height = 0.95,
 	freetype_load_flags = "NO_HINTING",
+	line_height = 0.95,
 	window_close_confirmation = "NeverPrompt",
 	window_padding = {
 		left = "0.5cell",
@@ -19,5 +20,4 @@ return {
 		top = 1,
 		bottom = "0.5cell",
 	},
-	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 }
