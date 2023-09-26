945917a1743edbe8d69ce6beae761a41f92232f9
Derek Passen <dpassen1@gmail.com>
2023-09-25 22:16:42 -0500

    emacs: This is freezing my emacs on suspend/minimize?

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -63,8 +63,7 @@
   (ring-bell-function 'ignore)
   (use-short-answers t)
   :init
-  (setq frame-title-format '("%b — emacs")
-        kill-buffer-query-functions nil
+  (setq kill-buffer-query-functions nil
         message-truncate-lines t
         read-process-output-max (* 1024 1024)))
 
