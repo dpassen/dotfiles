c5347365fc3de4c4113d75639250cc96b323213e
Derek Passen <dpassen1@gmail.com>
2023-11-22 19:41:15 -0600

    emacs: Switch to jimeh/emacs-builds

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -5,6 +5,7 @@
 
 (setq package-enable-at-startup nil)
 
+(push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
 
