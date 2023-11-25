31da4a364e305d4c034bf26aa0af828ab87d40b2
Derek Passen <dpassen1@gmail.com>
2023-11-25 13:07:33 -0600

    emacs: Merge early-init setqs

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -3,13 +3,12 @@
 
 ;;; Code:
 
-(setq package-enable-at-startup nil)
+(setq frame-inhibit-implied-resize t
+      package-enable-at-startup nil)
 
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
-
-(setq frame-inhibit-implied-resize t)
 
 (let ((default-gc-threshold gc-cons-threshold)
       (default-gc-percentage gc-cons-percentage)
