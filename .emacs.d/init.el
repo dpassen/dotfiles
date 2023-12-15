f4b7c1732c47968502ddb3fba31310bc545fc76f
Derek Passen <dpassen1@gmail.com>
2023-12-14 20:23:39 -0600

    emacs: Backport some TS/TSX setup from work

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -436,7 +436,7 @@
   :elpaca t
   :custom
   (treesit-auto-langs
-   '(bash c cpp css html java javascript json kotlin python ruby toml))
+   '(bash c cpp css html java javascript json kotlin python ruby toml tsx typescript))
   :hook (elpaca-after-init-hook . treesit-auto-add-to-auto-mode-alist))
 
 (use-package uniquify
