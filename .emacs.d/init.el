a386003944c4836f97adf9309f569aad0532c30d
Derek Passen <dpassen@ridehealth.com>
2023-12-28 11:04:52 -0600

    Line numbers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -122,7 +122,6 @@
   :ensure t
   :custom
   (consult-after-jump-hook '(recenter))
-  (consult-goto-line-numbers nil)
   (xref-show-definitions-function #'consult-xref)
   (xref-show-xrefs-function #'consult-xref)
   :general
@@ -167,6 +166,9 @@
 (use-package diredfl
   :ensure t
   :hook dired-mode-hook)
+
+(use-package display-line-numbers
+  :hook ((conf-mode-hook prog-mode-hook) . display-line-numbers-mode))
 
 (use-package dumb-jump
   :ensure t
@@ -354,7 +356,8 @@
   (modus-themes-fringes nil)
   (modus-themes-italic-constructs t)
   (modus-themes-mode-line '(accented borderless))
-  (modus-themes-region '(accented bg-only)))
+  (modus-themes-region '(accented bg-only))
+  (modus-themes-subtle-line-numbers t))
 
 (use-package mood-line
   :ensure t
