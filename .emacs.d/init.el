f02097666a25add19580fbeec70091ec8aca7259
Derek Passen <dpassen1@gmail.com>
2024-11-13 14:44:12 -0600

    Line numbers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -122,7 +122,6 @@
   :ensure t
   :custom
   (consult-after-jump-hook '(recenter))
-  (consult-goto-line-numbers nil)
   (xref-show-definitions-function #'consult-xref)
   (xref-show-xrefs-function #'consult-xref)
   :general
@@ -173,6 +172,9 @@
 (use-package diredfl
   :ensure t
   :hook dired-mode-hook)
+
+(use-package display-line-numbers
+  :hook ((conf-mode-hook prog-mode-hook) . display-line-numbers-mode))
 
 (use-package disproject
   :ensure t
@@ -368,7 +370,8 @@
   (modus-themes-fringes nil)
   (modus-themes-italic-constructs t)
   (modus-themes-mode-line '(accented borderless))
-  (modus-themes-region '(accented bg-only)))
+  (modus-themes-region '(accented bg-only))
+  (modus-themes-subtle-line-numbers t))
 
 (use-package mood-line
   :ensure t
