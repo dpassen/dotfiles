5abd5aaf9f0935302c582c30980fc96e1e3e6836
Derek Passen <dpassen1@gmail.com>
2025-02-23 11:39:05 -0600

    emacs: Bump elpaca

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,7 @@
 (setopt custom-file (locate-user-emacs-file "custom.el"))
 (load custom-file :no-error-if-file-is-missing)
 
-(defvar elpaca-installer-version 0.9)
+(defvar elpaca-installer-version 0.10)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
