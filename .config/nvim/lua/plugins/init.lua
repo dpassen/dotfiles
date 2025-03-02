6eb49b4062b578d7614f3916710ea4f502481419
Derek Passen <dpassen1@gmail.com>
2025-03-01 20:13:37 -0600

    Replace vim configuration with neovim
    - Add stylua and lua-language-server for editing lua files

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,91 @@
+return {
+	{
+		"miikanissi/modus-themes.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("modus")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = {},
+	},
+	{
+		"ibhagwan/fzf-lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = {
+			"skim",
+		},
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		config = {
+			cmdline = {
+				enabled = true,
+			},
+		},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+	},
+	{
+		"stevearc/oil.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = {},
+	},
+	{
+		"stevearc/conform.nvim",
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
+		config = {
+			formatters_by_ft = {
+				lua = {
+					"stylua",
+				},
+			},
+			format_on_save = {
+				timeout_ms = 500,
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"clojure",
+				"cpp",
+				"css",
+				"html",
+				"java",
+				"javascript",
+				"json",
+				"kotlin",
+				"lua",
+				"python",
+				"ruby",
+				"rust",
+				"toml",
+				"tsx",
+				"typescript",
+			},
+			sync_installed = false,
+			auto_install = true,
+			highlight = {
+				enabled = true,
+			},
+		},
+	},
+}
