1957b9421ff3a99f7986c063435539db3c9a4581
Derek Passen <dpassen1@gmail.com>
2025-12-15 12:04:10 -0600

    Remove unused plugins

--- leiningen/profiles.clj
+++ leiningen/profiles.clj
@@ -1,11 +1,4 @@
 #_{:clj-kondo/ignore [:unresolved-namespace]}
 {:user
  {:plugins
-  [[com.github.liquidz/antq "2.11.1276"]
-   [com.jakemccrary/lein-test-refresh "0.26.0"]
-   [lein-try/lein-try "0.4.3"]]
-  :test-refresh
-  {:banner       ""
-   :changes-only true
-   :clear        true
-   :quiet        true}}}
+  [[com.github.liquidz/antq "2.11.1276"]]}}
