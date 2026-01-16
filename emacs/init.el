7a63b1ad6e4d0f2d4e8c57829bf8ed49cd0bd01b
Derek Passen <dpassen1@gmail.com>
2026-01-16 13:57:24 -0600

    Add `nerd-icons-corfu`

--- emacs/init.el
+++ emacs/init.el
@@ -335,6 +335,10 @@
   :ensure t
   :hook (marginalia-mode-hook . nerd-icons-completion-marginalia-setup))
 
+(use-package nerd-icons-corfu
+  :ensure t
+  :hook (corfu-margin-formatters . nerd-icons-corfu-formatter))
+
 (use-package nerd-icons-dired
   :ensure t
   :hook dired-mode-hook)
