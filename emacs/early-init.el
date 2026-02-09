fb8cbc0a5c6c8acc47ff21e2193c9c6a4d75e7c6
Derek Passen <dpassen1@gmail.com>
2026-02-09 09:00:23 -0600

    Cleaner titlebars

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -8,6 +8,7 @@
       package-enable-at-startup nil)
 
 (push '(menu-bar-lines . 0) default-frame-alist)
+(push '(ns-transparent-titlebar . t) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
 
