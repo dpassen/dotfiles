149bd64282baaff60e8c2cb5dcf00ee1a7fee835
Derek Passen <dpassen1@gmail.com>
2026-02-19 09:03:47 -0600

    Hide frame until it's visible

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -4,17 +4,17 @@
 ;;; Code:
 
 (setq frame-inhibit-implied-resize t
+      menu-bar-mode nil
       mode-line-format nil
-      package-enable-at-startup nil)
+      package-enable-at-startup nil
+      scroll-bar-mode nil
+      tool-bar-mode nil)
 
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(ns-transparent-titlebar . t) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
-
-(setq menu-bar-mode nil
-      tool-bar-mode nil
-      scroll-bar-mode nil)
+(push '(visibility . nil) initial-frame-alist)
 
 (let ((default-file-name-handler-alist file-name-handler-alist)
       (default-gc-cons-percentage gc-cons-percentage)
@@ -29,7 +29,8 @@
               (setq file-name-handler-alist default-file-name-handler-alist
                     gc-cons-percentage default-gc-cons-percentage
                     gc-cons-threshold default-gc-cons-threshold
-                    vc-handled-backends default-vc-handled-backends))))
+                    vc-handled-backends default-vc-handled-backends)
+              (make-frame-visible))))
 
 ;; Local Variables:
 ;; no-byte-compile: t
