2aa585862d6b8c733b2a2ae9820aac42e408e483
Derek Passen <dpassen1@gmail.com>
2026-03-29 09:37:41 -0500

    Revert "Less distracting debug face"
    This reverts commit fc20b04f291e5684b63a5b915829d3466d2323e2.

--- emacs/init.el
+++ emacs/init.el
@@ -107,8 +107,6 @@
   (cider-save-file-on-load t)
   (cider-use-fringe-indicators nil)
   :custom-face
-  (cider-debug-code-overlay-face
-   ((t (:background unspecified))))
   (cider-result-overlay-face
    ((t (:background unspecified)))))
 
