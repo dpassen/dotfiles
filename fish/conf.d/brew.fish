0d74cd3f1177083accb79ba3c06384c70712ea31
Derek Passen <dpassen1@gmail.com>
2026-04-06 10:26:09 -0500

    Switch to global variables for user path entries

---
+++ fish/conf.d/brew.fish
@@ -0,0 +1,1 @@
+fish_add_path -ga /opt/homebrew/{,s}bin
--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,6 @@ set -q XDG_CONFIG_HOME; or set -gx XDG_CONFIG_HOME $HOME/.config
 set -q XDG_DATA_HOME; or set -gx XDG_DATA_HOME $HOME/.local/share
 set -q XDG_STATE_HOME; or set -gx XDG_STATE_HOME $HOME/.local/state
 
-fish_add_path -a /opt/homebrew/{,s}bin
-
 if status is-interactive
     set -q EDITOR; or set -gx EDITOR hx
 end
